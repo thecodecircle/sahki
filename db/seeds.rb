fd4726e1732a6a44f8da3b71e7e2404b10f0fdd6
Pashi <guynatan@gmail.com>
2020-08-05 01:26:54 +0300

    append

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
-puts 'Creating admin user...'
-Administrator.create_with(password: 'admin')
-  .find_or_create_by!(name: 'admin')
+# puts 'Creating admin user...'
+# Administrator.create_with(password: 'admin')
+#   .find_or_create_by!(name: 'admin')
+
+Game.create(status: "approved", name: "אדממה", description: "לרקוד ולשיר לפי השיר", how_to: "עומדים במעל ושרים את השיר", min_players: 5, max_players: 20)
+Game.create(status: "approved", name: "תופסת", description: "לרקוד ולשיר לפי השיר", how_to: "עומדים במעל ושרים את השיר", min_players: 5, max_players: 20)
+Game.create(status: "approved", name: "וואה", description: "לרקוד ולשיר לפי השיר", how_to: "עומדים במעל ושרים את השיר", min_players: 5, max_players: 20)
+Game.create(status: "approved", name: "מוקי צור בשדות הקטל", description: "לרקוד ולשיר לפי השיר", how_to: "עומדים במעל ושרים את השיר", min_players: 5, max_players: 20)
+Game.create(status: "approved", name: "אינטר גלקטיק סמוראי", description: "לרקוד ולשיר לפי השיר", how_to: "עומדים במעל ושרים את השיר", min_players: 5, max_players: 20)
