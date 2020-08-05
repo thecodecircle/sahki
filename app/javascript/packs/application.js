e15ed3cfbdfbb15f1696e116bb1c1847990f309b
Pashi <guynatan@gmail.com>
2020-08-05 14:05:09 +0300

    tinder

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,20 +1,7 @@
 import 'styles/application'
 import '../js/base'
+
 require("trix")
 require("@rails/actiontext")
 window.jQuery = $;
 window.$ = $;
-// import Swipe from 'swipejs'
-// const Swipe = require("swipejs")
-// require("jquery-ui")
-//
-// import '../js/jquery.ui.touch-punch.js'
-// import 'interactjs/interact'
-// require("interactjs")
-// import interact from 'https://cdn.jsdelivr.net/npm/interactjs@1.9.20/'
-// const interact = require('interactjs')
-// require('interactjs')
-
-// import interact from 'interactjs'
-
-import '../js/carousel'
