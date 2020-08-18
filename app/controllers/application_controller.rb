e168f2a5242478a4ace544cf6efebcdffc7985d5
Jordan <yarden11111@gmail.com>
2020-08-18 16:16:38 +0300

    Tags in game form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def current_admin
+    @_current_admin ||= session[:current_admin_id] && Administrator.find_by(id: session[:current_admin_id])
+  end
 end
